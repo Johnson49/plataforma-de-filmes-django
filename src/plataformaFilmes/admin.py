bbc28707d5f372bae31979b6af55f78467b16b3a
Simeone Johnson <108435026+Johnson49@users.noreply.github.com>
2022-09-11 20:45:08 -0300

    Add files via upload

---
+++ src/plataformaFilmes/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Filme, Avaliacao
+
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "ano_lancamento", "duracao", "criacao", "atualizacao", "ativo")
+
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('filme', 'nome', 'email', 'avaliacao', 'ativo')
